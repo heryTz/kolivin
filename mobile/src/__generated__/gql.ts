9d173c4ac23c19ed65b49539d5dde817e1545023
Nirintsoa <hhery889@gmail.com>
2022-12-29 14:09:13 +0300

    feat(mobile): compile typage

--- mobile/src/__generated__/gql.ts
+++ mobile/src/__generated__/gql.ts
@@ -1,19 +1,16 @@
 /* eslint-disable */
-import * as types from './graphql'
-import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core'
+import * as types from './graphql';
+import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
 
 const documents = {
-  '\n  query GetAds($sort: String, $offset: Int, $limit: Int) {\n    ads(sort: $sort, offset: $offset, limit: $limit) {\n      total,\n      data {\n        _id,\n        title\n      }\n    }\n  }\n':
-    types.GetAdsDocument,
-}
+    "\n  query GetAds($sort: String, $offset: Int, $limit: Int) {\n    ads(sort: $sort, offset: $offset, limit: $limit) {\n      total,\n      data {\n        _id,\n        title\n      }\n    }\n  }\n": types.GetAdsDocument,
+};
 
-export function gql(
-  source: '\n  query GetAds($sort: String, $offset: Int, $limit: Int) {\n    ads(sort: $sort, offset: $offset, limit: $limit) {\n      total,\n      data {\n        _id,\n        title\n      }\n    }\n  }\n',
-): typeof documents['\n  query GetAds($sort: String, $offset: Int, $limit: Int) {\n    ads(sort: $sort, offset: $offset, limit: $limit) {\n      total,\n      data {\n        _id,\n        title\n      }\n    }\n  }\n']
+export function gql(source: "\n  query GetAds($sort: String, $offset: Int, $limit: Int) {\n    ads(sort: $sort, offset: $offset, limit: $limit) {\n      total,\n      data {\n        _id,\n        title\n      }\n    }\n  }\n"): (typeof documents)["\n  query GetAds($sort: String, $offset: Int, $limit: Int) {\n    ads(sort: $sort, offset: $offset, limit: $limit) {\n      total,\n      data {\n        _id,\n        title\n      }\n    }\n  }\n"];
 
-export function gql(source: string): unknown
+export function gql(source: string): unknown;
 export function gql(source: string) {
-  return (documents as any)[source] ?? {}
+  return (documents as any)[source] ?? {};
 }
 
-export type DocumentType<TDocumentNode extends DocumentNode<any, any>> = TDocumentNode extends DocumentNode<infer TType, any> ? TType : never
+export type DocumentType<TDocumentNode extends DocumentNode<any, any>> = TDocumentNode extends DocumentNode<  infer TType,  any>  ? TType  : never;
