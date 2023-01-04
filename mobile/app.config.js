ec6d47733afc79d6bba7ac37bbf93a457bfdd8e4
Nirintsoa <hhery889@gmail.com>
2023-01-04 16:38:39 +0300

    test: versionCode manager

---
+++ mobile/app.config.js
@@ -0,0 +1,45 @@
+const IS_DEV = process.env.APP_VARIANT === 'development'
+const IS_PREVIEW = process.env.APP_VARIANT === 'preview'
+
+const APP_SUFFIX = IS_DEV ? 'dev' : IS_PREVIEW ? 'preview' : ''
+
+const APP_NAME_SUFFIX = APP_SUFFIX ? ` (${APP_SUFFIX})` : ''
+const APP_ID_SUFFIX = APP_SUFFIX ? `.${APP_SUFFIX}` : ''
+
+export default {
+  name: `kg-test${APP_NAME_SUFFIX}`,
+  slug: 'kg-test',
+  version: '1.0.6',
+  orientation: 'portrait',
+  icon: './assets/icon.png',
+  userInterfaceStyle: 'light',
+  splash: {
+    image: './assets/splash.png',
+    resizeMode: 'contain',
+    backgroundColor: '#ffffff',
+  },
+  updates: {
+    fallbackToCacheTimeout: 0,
+  },
+  assetBundlePatterns: ['**/*'],
+  ios: {
+    supportsTablet: true,
+    bundleIdentifier: `com.kgtest${APP_ID_SUFFIX}`,
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: './assets/adaptive-icon.png',
+      backgroundColor: '#FFFFFF',
+    },
+    package: `com.kgtest${APP_ID_SUFFIX}`,
+    versionCode: 7,
+  },
+  web: {
+    favicon: './assets/favicon.png',
+  },
+  extra: {
+    eas: {
+      projectId: 'f6bc349d-2aeb-414c-801b-81479c2c0338',
+    },
+  },
+}
